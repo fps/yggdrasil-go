9bf75979e6b39135d422cf88a04aab71104d8290
FPS <mista.tapas@gmx.net>
2022-01-24 15:21:14 +0100

    add some commandline args and remove some comments

--- cmd/ygglocaldns/main.go
+++ cmd/ygglocaldns/main.go
@@ -17,13 +17,19 @@ import (
 
 type args struct {
 	useconffile    string
+	port           int
+	address        string
 }
 
 func getArgs() args {
-	useconffile := flag.String("useconffile", "conf", "the config to use")
+	useconffile := flag.String("useconffile", "conf", "config file to read the private key from")
+	port := flag.Int("port", 55353, "port to listen on (UDP)")
+	address := flag.String("address", "", "the address to bind to")
 	flag.Parse()
 	return args{
 		useconffile: *useconffile,
+		port: *port,
+		address: *address,
 	}
 }
 
@@ -36,14 +42,13 @@ func parseQuery(m *dns.Msg) {
 			log.Printf("Query for %s\n", q.Name)
 			new_seed := make([]byte, 32)
 			name := q.Name[0:len(q.Name)-1]
-			log.Println(name)
 	        for index := 0; index < len(new_seed); index++ {
 				new_seed[index] = privateKey[index] ^ name[index % len(name)]
 			}
 
 			address := address.AddrForKey(ed25519.NewKeyFromSeed(new_seed).Public().(ed25519.PublicKey))
 			ip := net.IP(address[:]).String()
-			log.Printf(ip)
+			log.Println(ip)
 			if ip != "" {
 				rr, err := dns.NewRR(fmt.Sprintf("%s AAAA %s", q.Name, ip))
 				if err == nil {
@@ -55,7 +60,6 @@ func parseQuery(m *dns.Msg) {
 }
 
 func handleDnsRequest(w dns.ResponseWriter, r *dns.Msg) {
-	log.Printf("Request!\n")
 	m := new(dns.Msg)
 	m.SetReply(r)
 	m.Compress = false
@@ -86,8 +90,8 @@ func main() {
 	dns.HandleFunc(".", handleDnsRequest)
 
 	// start server
-	port := 55353
-	server := &dns.Server{Addr: ":" + strconv.Itoa(port), Net: "udp"}
+	port := args.port
+	server := &dns.Server{Addr: args.address + ":" + strconv.Itoa(port), Net: "udp"}
 	server.MsgAcceptFunc = func(dh dns.Header) dns.MsgAcceptAction {
 		return dns.MsgAccept
 	}
