492908148fd1c7eba9d610631f5ce9ce44ca5344
FPS <mista.tapas@gmx.net>
2022-02-02 16:33:49 +0100

    progress!

--- src/tuntap/iface.go
+++ src/tuntap/iface.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"strings"
 	"golang.org/x/net/dns/dnsmessage"
+	"github.com/google/gopacket"
 )	
 
 const TUN_OFFSET_BYTES = 4
@@ -26,60 +27,62 @@ func (tun *TunAdapter) read() {
 		if bs[24] == 0xff && bs[25] == 0x02 && bs[39] == 0xfb {
             var msg dnsmessage.Message
             err := msg.Unpack(bs[48:])
-            fmt.Println(msg)
-            if err != nil {
-                fmt.Println(err)
-            } else {
-                for _, q := range msg.Questions {
-                    fmt.Println("Question: ", q.Name.String())
-                    if strings.HasSuffix(q.Name.String(), ".ygg.local.") {
-                        fmt.Println("###### Looks like a real request")
-                        rsp := dnsmessage.Message{
-                            Header: dnsmessage.Header{Response: true, Authoritative: true},
-                            Answers: []dnsmessage.Resource{
-                                {
-                                    Header:  dnsmessage.ResourceHeader{
-                                        Name: q.Name,
-                                        Type: dnsmessage.TypeAAAA,
-                                        Class: dnsmessage.ClassINET,
-                                    },
-                                    Body: &dnsmessage.AAAAResource{ AAAA: [16]byte{1,2,3,4,5,6,7,8,9,10,11,12,13,14,15} },
-                                },
-                            },
-                        }
-                        rspbuf, _ := rsp.Pack()
-                        fmt.Println(rsp)
-                        c := make([]byte, 0, 1024)
-                        // Copy over original IPv6 header
-						c = append(c, 0x00, 0x00, 0x00, 0x00)
-                        c = append(c, bs[:48]...)
-                        // swap src and dst addresses
-                        // copy(c[24:], srcAddr[:])
-                        // copy(c[8:], dstAddr[:])
-                        c = append(c, rspbuf[:]...)
-                        l := len(c)
-                        // set IPv6 content length
-                        c[4+4] = byte(l-40)
-                        // set UDP content length
-                        c[44+4] = byte(l-48)
-                        // swap UDP ports
-                        // copy(c[40:42], bs[42:44])
-                        // copy(c[42:44], bs[40:42])
-                        // set UDP checksum to 0
-                        c[46+4] = 0
-                        c[47+4] = 0
-                        fmt.Println("sending answer, len: ", len(c))
-                        // fmt.Println(c)
-                        // _, _ = k.core.WriteTo(c[:], net.Addr{"udp", net.IP(dstAddr[:]).String()})
-                        // _, _ = k.readPC(c)
-                        // defer k.writePC(c)
-                        // k.sendToAddress(dstAddr, c)
-						// tun.iface.Write(c, 0)
-						tun.iface.Write(c[:], TUN_OFFSET_BYTES)
-                        fmt.Println("done")
+            // fmt.Println(msg)
+			if err != nil {
+				fmt.Println("Error unpacking: ", err)
+			} else {
+				for _, q := range msg.Questions {
+					if q.Type != dnsmessage.TypeAAAA { continue }
+					if !strings.HasSuffix(q.Name.String(), ".ygg.local.") { continue } 
+
+					fmt.Println("###### Looks like a real request")
+					var address [16]byte
+					copy(address[:], bs[8:24])
+					rsp := dnsmessage.Message{
+						Header: dnsmessage.Header{ ID: msg.Header.ID, Response: true, Authoritative: false },
+						Questions: []dnsmessage.Question{ q },
+						Answers: []dnsmessage.Resource{
+							{
+								Header:  dnsmessage.ResourceHeader{
+									//Name: dnsmessage.MustNewName("quark.bark.org."),
+									Name: q.Name,
+									Type: dnsmessage.TypeAAAA,
+									Class: dnsmessage.ClassINET,
+									TTL: 10,
+								},
+								// Body: &dnsmessage.AAAAResource{ AAAAA: [4]byte{ 127, 0, 0, 1} },
+								Body: &dnsmessage.AAAAResource{ AAAA: address },
+								//Body: &dnsmessage.AAAAResource{ AAAA: [16]byte{0xff,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15} },
+							},
+						},
 					}
+					rspbuf, err := rsp.Pack()
+					if  err != nil { fmt.Println("Error packing: ", err) }
+					fmt.Println(rsp.GoString())
+					c := make([]byte, 0, 1024)
+
+					// Copy over original IPv6 header
+					c = append(c, 0x00, 0x00, 0x00, 0x00)
+					c = append(c, bs[:48]...)
+					c = append(c, rspbuf[:]...)
+					l := len(rspbuf)
+
+					// set IPv6 content length
+					c[5+4] = byte(l+8)
+					c[4+4] = 0x00
+
+					// set UDP content length
+					c[45+4] = byte(l+8)
+					c[44+4] = 0x00
+
+					// set UDP checksum
+					c[46+4] = 0xff
+					c[47+4] = 0xff
+
+					tun.iface.Write(c[:], TUN_OFFSET_BYTES)
+					fmt.Println("done")
 				}
-			}	
+			}
 		}
 		if _, err := tun.rwc.Write(bs); err != nil {
 			tun.log.Debugln("Unable to send packet:", err)
